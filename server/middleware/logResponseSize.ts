afed00f72ff14e8236213b1797dd123154f890b2
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-11-08 09:12:01 +0200

    fix: log responses <1 MiB as KiB
    small responses would get logged as 0.00 MiB

--- server/middleware/logResponseSize.ts
+++ server/middleware/logResponseSize.ts
@@ -10,7 +10,12 @@ export default defineEventHandler((event) => {
 	event.node.res.end = function (chunk?: any, ...args: any[]) {
 		if (chunk) {
 			const size = Buffer.byteLength(chunk);
-			console.log(`Response size: ${(size / 1024 ** 2).toFixed(2)} MiB`);
+
+			if (size < 1024 * 1024) {
+				console.log(`Response size: ${(size / 1024).toFixed(2)} KiB`);
+			} else {
+				console.log(`Response size: ${(size / 1024 ** 2).toFixed(2)} MiB`);
+			}
 		}
 		return originalEnd.call(this, chunk, ...(args as [any]));
 	};
